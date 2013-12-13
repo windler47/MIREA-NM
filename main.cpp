2e56efd13c0bf29005761a8a84a289a692fe6bdc
windler47 <scoundrelalva@gmail.com>
2013-12-14 01:19:39 +0400

    my_exp done!

--- main.cpp
+++ main.cpp
@@ -15,35 +15,48 @@ int main(int argc, char *argv[])
     std::string digits[] = {"1","2","3","4","5","6","7","8","9"};
     bool wrong_param = false;
     int result = 1;
-    if (argc>1) {
-        if ( !digits[0].compare(argv[1]) || !digits[1].compare(argv[1]) ) {
+    if (argc>1)
+    {
+        if ( !digits[0].compare(argv[1]) || !digits[1].compare(argv[1]) )
+        {
             cerr << "no program for this lab";
             return 2;
-        } else if ( !digits[2].compare(argv[1]) ) {
+        }
+        else if ( !digits[2].compare(argv[1]) )
+        {
             string functions[] = {"sin","cos","exp","ln","lg","log"};
             wrong_param = true;
-            for (int i=0; i<5; i++) {
-                if ( !functions[i].compare(argv[2]) && argc>3 ) {
+            for (int i=0; i<5; i++)
+            {
+                if ( !functions[i].compare(argv[2]) && argc>3 )
+                {
                     result = lab3(functions[i], strtod(argv[3],nullptr), strtod(argv[4],nullptr));
                     wrong_param = false;
                 }
             }
-            if ( !functions[5].compare(argv[2]) && argc>4 ) {
+            if ( !functions[5].compare(argv[2]) && argc>4 )
+            {
                 result = lab3(functions[5], strtod(argv[3],nullptr), strtod(argv[4],nullptr), strtod(argv[5],nullptr));
                 wrong_param = false;
             }
-        } else {
+        }
+        else
+        {
             wrong_param = true;
         }
-    } else {
+    }
+    else
+    {
         wrong_param = true;
     }
-    switch (result) {
+    switch (result)
+    {
     case 1:
         wrong_param = true;
     }
 
-    if (wrong_param) {
+    if (wrong_param)
+    {
         print_help();
         return 1;
     }
--- my_math.hpp
+++ my_math.hpp
@@ -10,37 +10,45 @@ double my_ln(double x, double e);
 double my_lg(double x, double e);
 double my_log(double a, double x, double e);
 
-double my_sin(double x, double e){
+double my_sin(double x, double e)
+{
     if (e >= 0.5) return 0.5;
     double result = 0;
     int_fast8_t sign = 1;
     //make argument positive
-    if ( x<0 ) {
+    if ( x<0 )
+    {
         sign*= (-1);
         x=-x;
     }
     //make argument < 2 pi
     while (x>2*M_PI) x-=2*M_PI;
 
-    if ( x > 3 * M_PI_4){
+    if ( x > 3 * M_PI_4)
+    {
         x = 2*M_PI - x;
         sign*= (-1);
     }
-    else if ( x > M_PI ){
+    else if ( x > M_PI )
+    {
         x-=M_PI;
         sign*= (-1);
     }
-    else if ( x > M_PI_2 ){
+    else if ( x > M_PI_2 )
+    {
         x = abs(x-M_PI);
     }
-    if ( x > M_PI_4 ){
+    if ( x > M_PI_4 )
+    {
         result = my_cos(x - M_PI_4 , e);
     }
-    else{
+    else
+    {
         int den = 1;
         double xp = x;
         result = x;
-        while ( xp/den >= e ){
+        while ( xp/den >= e )
+        {
             den *= (den+1) * (den+2);
             xp *= (-1)*x*x;
             result+=xp/den;
@@ -49,36 +57,44 @@ double my_sin(double x, double e){
     return result;
 }
 
-double my_cos(double x, double e){
+double my_cos(double x, double e)
+{
     if (e >= 0.5) return 0.5;
     double result = 0;
     int_fast8_t sign = 1;
     //make argument positive
-    if ( x<0 ) {
+    if ( x<0 )
+    {
         x=-x;
     }
     //make argument < 2 pi
     while (x>2*M_PI) x-=2*M_PI;
 
-    if ( x > 3 * M_PI_4){
+    if ( x > 3 * M_PI_4)
+    {
         x = 2*M_PI - x;
     }
-    else if ( x > M_PI ){
+    else if ( x > M_PI )
+    {
         x-=M_PI;
         sign*= (-1);
     }
-    else if ( x > M_PI_2 ){
+    else if ( x > M_PI_2 )
+    {
         x = abs(x-M_PI);
         sign*= (-1);
     }
-    if ( x > M_PI_4 ){
+    if ( x > M_PI_4 )
+    {
         result = my_sin(x - M_PI_4 , e);
     }
-    else{
+    else
+    {
         int den = 2;
         double xp = (-x)*x;
         result = 1 + xp/den ;
-        while ( xp/den >= e ){
+        while ( xp/den >= e )
+        {
             den *= (den+1) * (den+2);
             xp *= (-1)*x*x;
             result+=xp/den;
@@ -87,29 +103,56 @@ double my_cos(double x, double e){
     return result;
 }
 
-double my_exp(double x, double e){
+double my_exp(double x, double e)
+{
+    double result;
+    if (x<0) return 1/my_exp(abs(x),e);
+    if (x==0) return 1;
+    /*    if (x>1){
+            double frac_part;
+            int int_part = modf(x,&frac_part);
+            result = my_pow(my_exp( M_E , int_part ) * my_exp(frac_part,e)
+        } //Bad medicine (Don't know how to determine error
+        */
+    int den = 0;
+    int denf = 1;
+    double xp = 1;
+    result = xp/denf;
+    while (xp/den>=e)
+    {
+        xp*=x;
+        den+=1;
+        denf*=den;
+        result+=xp/denf;
+    }
+    return result;
+}
+
+double my_ln(double x, double e)
+{
     return 0;
 }
 
-double my_ln(double x, double e){
+double my_lg(double x, double e)
+{
     return 0;
 }
 
-double my_lg(double x, double e){
+double my_log(double a, double x, double e)
+{
     return 0;
 }
 
-double my_log(double a, double x, double e){
+double my_pow(double x, double y)
+{
     return 0;
 }
-
-double my_pow(double x, double y){
-    return 0;
-}
-double my_pow(double x, int n){
+double my_pow(double x, int n)
+{
     if (x==0) return 0;
     if ( n<0 ) return 1/my_pow(x,-n);
-    switch (n){
+    switch (n)
+    {
     case 0:
         return 1;
     case 1:
@@ -117,10 +160,12 @@ double my_pow(double x, int n){
     case 2:
         return x*x;
     default:
-        if(n%2==0){
+        if(n%2==0)
+        {
             return my_pow(x*x,(int)n/2);
         }
-        else{
+        else
+        {
             return x*my_pow(x*x,(int)((n-1)/2));
         }
     }
