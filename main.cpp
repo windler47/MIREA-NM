09d51bd8c758e38f8d01ebd568bfb66b50920a8f
windler47 <scoundrelalva@gmail.com>
2013-12-15 06:17:48 +0400

    math.hpp Done

--- main.cpp
+++ main.cpp
@@ -14,82 +14,65 @@ void print_help();
 
 int main(int argc, char *argv[])
 {
-    double x = -50;
-    double dx = 1/(double)10000;
-    double maxdelta = 0;
-    double my = 0;
-    double st = 0;
-    for (int i=0; i<1000000; i++)
+    std::string digits[] = {"1","2","3","4","5","6","7","8","9"};
+    bool wrong_param = false;
+    int result = 1;
+    if (argc>1)
     {
-        my = my_sin(x,0.01);
-        st = sin(x);
-        if (abs(my-st)>maxdelta)
+        if ( !digits[0].compare(argv[1]) || !digits[1].compare(argv[1]) )
         {
-            maxdelta = my-st;
-            cout << "For x="<<x<<" delta="<<maxdelta << "\n";
+            cerr << "no program for this lab";
+            return 2;
         }
-        x+=dx;
+        else if ( !digits[2].compare(argv[1]) )
+        {
+            string functions[] = {"sin","cos","exp","ln","lg","log"};
+            wrong_param = true;
+            for (int i=0; i<5; i++)
+            {
+                if ( !functions[i].compare(argv[2]) && argc>3 )
+                {
+                    result = lab3(functions[i], strtod(argv[3],nullptr), strtod(argv[4],nullptr));
+                    wrong_param = false;
+                }
+            }
+            if ( !functions[5].compare(argv[2]) && argc>4 )
+            {
+                result = lab3(functions[5], strtod(argv[3],nullptr), strtod(argv[4],nullptr), strtod(argv[5],nullptr));
+                wrong_param = false;
+            }
+        }
+        else
+        {
+            wrong_param = true;
+        }
     }
-    cout << "max delta = "<<maxdelta << "\n";
-//    std::string digits[] = {"1","2","3","4","5","6","7","8","9"};
-//    bool wrong_param = false;
-//    int result = 1;
-//    if (argc>1)
-//    {
-//        if ( !digits[0].compare(argv[1]) || !digits[1].compare(argv[1]) )
-//        {
-//            cerr << "no program for this lab";
-//            return 2;
-//        }
-//        else if ( !digits[2].compare(argv[1]) )
-//        {
-//            string functions[] = {"sin","cos","exp","ln","lg","log"};
-//            wrong_param = true;
-//            for (int i=0; i<5; i++)
-//            {
-//                if ( !functions[i].compare(argv[2]) && argc>3 )
-//                {
-//                    result = lab3(functions[i], strtod(argv[3],nullptr), strtod(argv[4],nullptr));
-//                    wrong_param = false;
-//                }
-//            }
-//            if ( !functions[5].compare(argv[2]) && argc>4 )
-//            {
-//                result = lab3(functions[5], strtod(argv[3],nullptr), strtod(argv[4],nullptr), strtod(argv[5],nullptr));
-//                wrong_param = false;
-//            }
-//        }
-//        else
-//        {
-//            wrong_param = true;
-//        }
-//    }
-//    else
-//    {
-//        wrong_param = true;
-//    }
-//    switch (result)
-//    {
-//    case 1:
-//        wrong_param = true;
-//    }
-//
-//    if (wrong_param)
-//    {
-//        print_help();
-//        return 1;
-//    }
+    else
+    {
+        wrong_param = true;
+    }
+    switch (result)
+    {
+    case 1:
+        wrong_param = true;
+    }
+
+    if (wrong_param)
+    {
+        print_help();
+        return 1;
+    }
 
     return 0;
 }
 
 int lab3(string fun, double x, double e, double a)
 {
-    cout << "function: "<< fun << "\n";
+    cout << "function: " << fun << "\n";
     cout << "argument = " << x << "\n";
     cout << "epsilon = " << e << "\n";
     cout << "optional argument = " << a;
-    double result = my_sin(5,0.001);
+    double result = my_sin(5, 0.001);
     cout << "M_PI = " << result;
     return 0;
 }
--- my_math.hpp
+++ my_math.hpp
@@ -10,145 +10,114 @@ double my_pow(double x, int n);
 double my_pow(double x, double y);
 double my_ln(double x, double e);
 double my_lg(double x, double e);
-double my_log(double a, double x, double e);
+double my_log(double x, double a, double e);
 
 double my_sin(double x, double e)
 {
-    if (e >= 0.5) return 0.5;
+    if (e >= 0.5) {
+        return 0.5;
+    }
     double result = 0;
     int_fast8_t sign = 1;
     //make argument positive
-    if ( x<0 )
-    {
-        if (fabs(sin(x)+sin(-x))<e)
-        {
-            sign*= (-1);
-            x=-x;
-        }
-        else std::cout<<"You are wrong!";
+    if ( x < 0 ) {
+        sign *= (-1);
+        x = -x;
     }
     //make argument < 2 pi
-    while (x>2*M_PI) x-=2*M_PI;
-    if (x>M_PI)
-    {
-        if (fabs(sin(x)+sin(x-M_PI))<e)
-        {
-            x-=M_PI;
-            sign*=(-1);
-        }
-        else std::cout<<"You are wrong!";
+    while (x > 2 * M_PI) {
+        x -= 2 * M_PI;
     }
-    if (x>M_PI_2)
-    {
-        if( (fabs(sin(x) - sin(fabs(x-M_PI))))<e)
-        {
-            x=fabs(x-M_PI);
-        }
-        else std::cout<<"You are wrong!";
+    if (x > M_PI) {
+        x -= M_PI;
+        sign *= (-1);
     }
-    if (x>M_PI_4)
-    {
-        if (fabs(sin(x)-cos(M_PI_2-x))<e)
-        {
-            result=my_cos(M_PI_2-x,e);
-        }
-        else std::cout<<"You are wrong!";
+    if (x > M_PI_2) {
+        x = fabs(x - M_PI);
     }
-    else
-    {
-//        result = sin(x);
+    if (x > M_PI_4) {
+        result = my_cos(M_PI_2 - x, e);
+    } else {
         int den = 1;
         double denf = 1;
         double xp = x;
         result = x;
-        while ( fabs(xp/denf) > e )
-        {
-            denf *= (den+1) * (den+2);
-            den+=2;
-            xp *= (-1)*x*x;
-            result+=xp/denf;
-        }
-        if (fabs(sin(x)-result)>e) {
-                std::cout<<"bad series!";
+        while ( fabs(xp / denf) > e ) {
+            denf *= (den + 1) * (den + 2);
+            den += 2;
+            xp *= (-1) * x * x;
+            result += xp / denf;
         }
     }
-    return sign*result;
+    return sign * result;
 }
 
 double my_cos(double x, double e)
 {
-    return cos(x);
-//    if (e >= 0.5) return 0.5;
-//
-//    double result = 0;
-//    int_fast8_t sign = 1;
-//    //make argument positive
-//    if ( x<0 )
-//    {
-//        x=fabs(x);
-//    }
-//    //make argument < 2 pi
-//    while (x>2*M_PI) x-=2*M_PI;
-//    if (x>M_PI)
-//    {
-//        x-=M_PI;
-//        sign*=(-1);
-//    }
-//    if (x>M_PI_2)
-//    {
-//        if( (fabs(cos(x) + cos(fabs(x-M_PI))))<e)
-//        {
-//            x=fabs(x-M_PI);
-//            sign*=(-1);
-//        }
-//        else std::cout<<"You are wrong!";
-//    }
-//    if (x>M_PI_4)
-//    {
-//        if (fabs(cos(x)-sin(M_PI_2-x))<e)
-//        {
-//            result=my_sin(M_PI_2-x,e);
-//        }
-//        else std::cout<<"You are wrong!";
-//    }
-//    else
-//    {
-//        int den = 2;
-//        double denf = 2;
-//        double xp = (-x)*x;
-//        result = 1 + xp/denf ;
-//        while ( fabs(xp/denf) >= e )
-//        {
-//            denf *= (den+1) * (den+2);
-//            den+=2;
-//            xp *= (-1)*x*x;
-//            result+=xp/denf;
-//        }
-//    }
-//    return result;
+    if (e >= 0.5) {
+        return 0.5;
+    }
+
+    double result = 0;
+    int_fast8_t sign = 1;
+    //make argument positive
+    if ( x < 0 ) {
+        x = -x;
+    }
+    //make argument < 2 pi
+    while (x > 2 * M_PI) {
+        x -= 2 * M_PI;
+    }
+
+    if (x > M_PI) {
+        x -= M_PI;
+        sign *= (-1);
+    }
+    if (x > M_PI_2) {
+        x = fabs(x - M_PI);
+        sign *= (-1);
+    }
+    if (x > M_PI_4) {
+        result = my_sin(M_PI_2 - x, e);
+    } else {
+        int den = 2;
+        double denf = 2;
+        double xp = (-x) * x;
+        result = 1 + xp / denf ;
+        while ( fabs(xp / denf) >= e ) {
+            denf *= (den + 1) * (den + 2);
+            den += 2;
+            xp *= (-1) * x * x;
+            result += xp / denf;
+        }
+    }
+    return sign * result;
 }
 
 double my_exp(double x, double e)
 {
     double result;
-    if (x<0) return 1/my_exp(abs(x),e);
-    if (x==0) return 1;
-    /*    if (x>1){
-            double frac_part;
-            int int_part = modf(x,&frac_part);
-            result = my_pow(my_exp( M_E , int_part ) * my_exp(frac_part,e)
-        } //Bad medicine (Don't know how to determine error
-        */
-    int den = 0;
-    double denf = 1;
-    double xp = 1;
-    result = xp/denf;
-    while (abs(xp/denf)>=e)
-    {
-        xp*=x;
-        den++;
-        denf*=den;
-        result+=xp/denf;
+    if (x == 0) {
+        return 1;
+    }
+    if (x < 0) {
+        result = 1 / my_exp(fabs(x), e);
+    } else if (x > 1) {
+        double int_part ;
+        double frac_part = modf(x, &int_part);
+        e = e / int_part;
+        result = my_pow( M_E , (int)int_part ) * my_exp( frac_part, e);
+    } else {
+        int den = 0;
+        double denf = 1;
+        double xp = 1;
+        result = xp / denf;
+        while (fabs(xp / denf) >= e) {
+            xp *= x;
+            den++;
+            denf *= den;
+            result += xp / denf;
+        }
     }
     return result;
 }
@@ -156,69 +125,80 @@ double my_exp(double x, double e)
 double my_ln(double x, double e)
 {
     double result = 0;
-    if (x==0) return std::numeric_limits<double>::signaling_NaN();
-    if (x==1) return 0;
-    if (abs(x)<1)
-    {
-        double y = x-1;
-        double yp = y;
-        double den = 1;
-        result = yp/den;
-        std::cout << abs(yp/den);
-        while (abs(yp/den) > e)
-        {
-            den = (-1)*(den+1);
-            yp*=y;
-            result += yp/den;
-        }
+    if (x <= 0) {
+        return std::numeric_limits<double>::signaling_NaN();
     }
-    else
-    {
+    if (x == 1) {
+        return 0;
+    }
+    if (x <= 2) {
+        x--;
+        int den = 1;
+        double xp = result = x;
+        while (fabs(xp / den) > e) {
+            xp *= (-1) * x;
+            den++;
+            result += xp / den;
+        }
+    } else {
         int tp = 0;
-        while (x>1)
-        {
-            x = x/10;
+        while (x > 1) {
+            x = x / 10;
             tp++;
         }
-        result = my_ln(x,e)+tp*M_LN10;
+        result = my_ln(x, e) + tp * M_LN10;
     }
-    return 0;
+    return result;
 }
 
 double my_lg(double x, double e)
 {
-    return 0;
+    return my_ln(x, e) / M_LN10;
 }
 
-double my_log(double a, double x, double e)
+double my_log(double x, double a, double e)
 {
-    return 0;
+    return my_ln(x, e) / my_ln(a, e );
 }
 
 double my_pow(double x, double y)
 {
-    return 0;
+    if (x == 0) {
+        return 0;
+    }
+    if ( y < 0 ) {
+        return 1 / my_pow(x, -y);
+    }
+    if (y == 0) { return 1; }
+    double result;
+    double int_part ;
+    double frac_part = modf(y, &int_part);
+    if (frac_part != 0 && x < 0 ) { result = std::numeric_limits<double>::signaling_NaN(); }
+    else if (int_part != 0) { result = my_pow(x, (int)int_part) * my_pow(x, frac_part); }
+    else {
+        result = exp(y * log(x));
+    }
 }
 double my_pow(double x, int n)
 {
-    if (x==0) return 0;
-    if ( n<0 ) return 1/my_pow(x,-n);
-    switch (n)
-    {
+    if (x == 0) {
+        return 0;
+    }
+    if ( n < 0 ) {
+        return 1 / my_pow(x, -n);
+    }
+    switch (n) {
     case 0:
         return 1;
     case 1:
         return x;
     case 2:
-        return x*x;
+        return x * x;
     default:
-        if(n%2==0)
-        {
-            return my_pow(x*x,(int)n/2);
-        }
-        else
-        {
-            return x*my_pow(x*x,(int)((n-1)/2));
+        if(n % 2 == 0) {
+            return my_pow(x * x, (int)n / 2);
+        } else {
+            return x * my_pow(x * x, (int)((n - 1) / 2));
         }
     }
 }
